e18f8e6b2f661abe551c7c170646263386eec946
Steve Klabnik <steve@steveklabnik.com>
2018-12-05 16:01:45 -0500

    some fixes and some layout stuff

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ use std::{
 
 use handlebars::Handlebars;
 
+use serde_derive::{Deserialize, Serialize};
+
 struct Blog {
     handlebars: Handlebars,
     posts: Vec<Post>,
@@ -21,6 +23,13 @@ struct Post {
     year: String,
     month: String,
     day: String,
+    contents: String,
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+struct YamlHeader {
+    title: String,
+    author: String,
 }
 
 impl Blog {
@@ -53,6 +62,7 @@ impl Blog {
             // yeah this might blow up, but it won't
             let filename = path.file_name().unwrap().to_str().unwrap();
 
+            // we need to get the metadata out of the url
             let mut split = filename.splitn(4, "-");
 
             let year = split.next().unwrap().to_string();
@@ -60,8 +70,21 @@ impl Blog {
             let day = split.next().unwrap().to_string();
             let filename = split.next().unwrap().to_string();
 
+            // now we need to get the data from the post itself
             let author = String::from("steve");
             let title = String::from(&*filename);
+
+            let contents = fs::read_to_string(path)?;
+
+            // yaml headers.... we know the first four bytes of each file are "---\n"
+            // so we need to find the end. we need the fours to adjust for those first bytes
+            let end_of_yaml = contents[4..].find("---").unwrap() + 4;
+            let yaml = &contents[..end_of_yaml];
+
+            let YamlHeader { author, title } = serde_yaml::from_str(yaml)?;
+
+            // finally, the contents. we add + to get rid of the final "---\n\n"
+            let contents = contents[end_of_yaml + 5..].to_string();
 
             let post = Post {
                 filename,
@@ -70,10 +93,13 @@ impl Blog {
                 year,
                 month,
                 day,
+                contents,
             };
 
-            println!("Found post: {:?}", post);
+            posts.push(post);
         }
+
+        // finally, sort the posts. oldest first.
 
         Ok(posts)
     }
@@ -89,17 +115,15 @@ impl Blog {
     fn render_index(&self) -> Result<(), Box<Error>> {
         let mut data = BTreeMap::new();
         data.insert("title".to_string(), "The Rust Programming Language Blog".to_string());
+        data.insert("parent".to_string(), "layout".to_string());
 
-        self.render_template("index.md", "index", data)?;
+        self.render_template("index.html", "index", data)?;
         
         Ok(())
     }
 
     fn render_template(&self, name: &str, template: &str, data: BTreeMap<String, String>) -> Result<(), Box<Error>> {
-        let mut html = PathBuf::from(name);
-        html.set_extension("html");
-
-        let out_file = self.out_directory.join(html);
+        let out_file = self.out_directory.join(name);
 
         let file = File::create(out_file)?;
 
